b6b5374af20110d2981894560b827748ea82b01b
Thebowers89 <thebowers89@gmail.com>
2015-07-15 21:23:53 -0400

    Added two more commands

--- src/DA/Commands/ClassCommand.java
+++ src/DA/Commands/ClassCommand.java
@@ -33,7 +33,9 @@ public class ClassCommand implements CommandExecutor {
                     ItemStack sword = new ItemStack(Material.DIAMOND_SWORD, 1);
                     sword.addEnchantment(Enchantment.DAMAGE_UNDEAD, 2);
                     sword.addEnchantment(Enchantment.DAMAGE_ALL, 4);
+                    ItemStack food = new ItemStack(Material.COOKED_BEEF, 32);
 
+                    pi.setItem(1, food);
                     pi.setItem(39, helmet);
                     pi.setItem(38, chest);
                     pi.setItem(37, leg);
@@ -68,6 +70,58 @@ public class ClassCommand implements CommandExecutor {
                     pi.setItem(39, helmet);
 
                     sender.sendMessage(ChatColor.GREEN + "You have chosen the Ranger Class");
+                    return true;
+                }
+                if (args[0].equalsIgnoreCase("alchemist")) {
+                    Inventory pi = player.getInventory();
+
+                    ItemStack helmet = new ItemStack(Material.LEATHER_HELMET, 1);
+                    helmet.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                    helmet.addEnchantment(Enchantment.DURABILITY, 3);
+                    ItemStack chest = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
+                    chest.addEnchantment(Enchantment.DURABILITY, 3);
+                    chest.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                    ItemStack leg = new ItemStack(Material.LEATHER_LEGGINGS, 1);
+                    leg.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                    leg.addEnchantment(Enchantment.DURABILITY, 3);
+                    ItemStack boot = new ItemStack(Material.LEATHER_BOOTS, 1);
+                    boot.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                    boot.addEnchantment(Enchantment.DURABILITY, 3);
+                    ItemStack sword = new ItemStack(Material.WOOD_SWORD,1 );
+                    sword.addEnchantment(Enchantment.DAMAGE_ALL, 4);
+                    ItemStack gapple = new ItemStack(Material.GOLDEN_APPLE, 4,(short) 1);
+
+                    ItemStack feye = new ItemStack(Material.FERMENTED_SPIDER_EYE, 8);
+                    ItemStack bottle = new ItemStack(Material.GLASS_BOTTLE, 24);
+                    ItemStack eye = new ItemStack(Material.SPIDER_EYE, 4);
+                    ItemStack melon = new ItemStack(Material.SPECKLED_MELON, 8);
+                    ItemStack feather = new ItemStack(Material.FEATHER, 4);
+                    ItemStack cream = new ItemStack(Material.MAGMA_CREAM, 2);
+                    ItemStack rstone = new ItemStack(Material.REDSTONE, 24);
+                    ItemStack powder = new ItemStack(Material.SULPHUR, 24);
+                    ItemStack gcarrot = new ItemStack(Material.GOLDEN_CARROT, 1);
+                    ItemStack tear = new ItemStack(Material.GHAST_TEAR, 4);
+                    ItemStack bpowder = new ItemStack(Material.BLAZE_POWDER, 2);
+
+                    pi.setItem(9, feye);
+                    pi.setItem(10, bottle);
+                    pi.setItem(11, eye);
+                    pi.setItem(12, melon);
+                    pi.setItem(13, feather);
+                    pi.setItem(14, cream);
+                    pi.setItem(15, rstone);
+                    pi.setItem(16, powder);
+                    pi.setItem(17, gcarrot);
+                    pi.setItem(18, tear);
+                    pi.setItem(19, bpowder);
+
+                    pi.setItem(1, gapple);
+                    pi.setItem(0, sword);
+                    pi.setItem(36, boot);
+                    pi.setItem(37, leg);
+                    pi.setItem(38, chest);
+                    pi.setItem(39, helmet);
+
                     return true;
                 }
                 if (args[0].equalsIgnoreCase("dalek")) {
